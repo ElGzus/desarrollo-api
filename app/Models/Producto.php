7a50b19e6ecc2802203e216d63b466507e8f9336
Abraham <josuevasquezdd@gmail.com>
2024-07-14 12:10:29 -0600

    Added 'valoraciones' relacion al Producto model

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Producto extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+        'precio',
+    ];
+
+    public function valoraciones()
+    {
+        return $this->hasMany(Valoracion::class);
+    }
+
 }
